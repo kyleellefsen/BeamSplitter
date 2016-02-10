08af61c492ccdd85a6869e6a6211e3709cfbdd8c
Brett Settle <brettjsettle@gmail.com>
2016-02-09 22:17:19 -0800

    new init

--- __init__.py
+++ __init__.py
@@ -1,9 +1,20 @@
-{'name': 'Beam Splitter',
-'author': 'Brett Settle',
-'base_dir': 'BeamSplitter',
-'date': '2/2/2016',
-'dependencies': [],
-'url': 'http://github.com/BrettJSettle/BeamSplitter/archive/master.zip',
-'description': 'Beam Splitter plugin for aligning images to link them and analyze copied ROIs',
-'menu_layout': {'Beam Splitter': ['beam_splitter', 'beam_splitter.gui']},
-}
+<plugin name=Beam Splitter>
+	<author>
+		Brett Settle
+	</author>
+	<base_dir>
+		BeamSplitter
+	</base_dir>
+	<date>
+		02/2/2016
+	</date>
+	<url>
+		http://github.com/BrettJSettle/BeamSplitter/archive/master.zip
+	</url>
+	<description>
+		Beam Splitter plugin for aligning images to link them and analyze copied ROIs
+	</description>
+	<menu_layout>
+		<action location="beam_splitter" function="beam_splitter.gui">Beam Splitter Dialog</action>
+	</menu_layout>
+</plugin>
